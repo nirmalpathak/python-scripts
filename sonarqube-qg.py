ed9f978cc9b221124fa00e6fb73bfaf56ae2f536
Nirmal Pathak <ndpathak@yahoo.com>
2017-05-25 17:09:25 +0530

    Initial Commit

---
+++ sonarqube-qg.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os, sys, json, requests
+
+arg = sys.argv[1]
+
+def get_status(status):
+        staturl = "https://sonar.domain.net/api/ce/activity?status=IN_PROGRESS&q=com.domain:"+arg
+        response = requests.get(staturl, auth=(os.environ['SONAR_USERNAME'], os.environ['SONAR_PASSWORD']))
+        data = response.json()
+        t = data['tasks']
+        return len(t)
+
+def get_qg(proj):
+        qgurl = "https://sonar.domain.net/api/qualitygates/project_status?projectKey=com.domain:"+proj
+        qgresp = requests.get(qgurl, auth=(os.environ['SONAR_USERNAME'], os.environ['SONAR_PASSWORD']))
+        qg = qgresp.json()
+        return qg['projectStatus']['status']
+
+t = get_status(arg)
+print t
+if t != 0:
+        print ("SonarQube Background Task is in progress!")
+        while t != 0:
+                #print ("Background Task is in progress!")
+                t = get_status(arg)
+                #print t
+
+        qgstatus = get_qg(arg)
+        print qgstatus
+        if qgstatus == 'OK':
+                print '\033[1;32mQuality Gate PASSED!\033[1;m'
+                sys.exit(0)
+        else:
+                print '\033[1;31mQuality Gate FAILED!\033[1;m'
+                sys.exit(127)
+else:
+        qgstatus = get_qg(arg)
+        print qgstatus
+        if qgstatus == 'OK':
+                print '\033[1;32mQuality Gate PASSED!\033[1;m'
+                sys.exit(0)
+        else:
+                print '\033[1;31mQuality Gate FAILED!\033[1;m'
+                sys.exit(127)
