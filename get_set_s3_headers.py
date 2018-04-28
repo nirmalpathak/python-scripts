f1e05d765120eb2159a23330961378b3eb4a77de
Nirmal Pathak <ndpathak@yahoo.com>
2018-04-28 07:28:57 +0530

    Updated if condition.

--- get_set_s3_headers.py
+++ get_set_s3_headers.py
@@ -24,17 +24,8 @@ def main():
 	if key is not None:
             print key
             #print key.cache_control, key.content_type, key.expires
-            if key.cache_control is None and key.expires is None:
-                print "\033[1;31mNo Cache Control & Expires headers set!\033[1;m"
-                metadata = key.metadata
-                metadata['Cache-Control'] = 'max-age=%d, public' % (60 * 60 * 24 * 180) # Set Cache-Control Value to 60 x 60 x 24 x 180 = 6 Months
-                metadata['Expires'] = 'Fri, 15 Feb 2019 16:00:00 GMT' # Setting Expires Metadata date & time.
-                metadata['Content-Type'] =  key.content_type # Keeping existing Content-Type.
-                key.copy(AWS_BUCKET_NAME, key, metadata=metadata, preserve_acl=True)
-                print metadata
-                print key.name, key.cache_control, key.content_type, key.expires
-            elif key.expires is None or key.cache_control is None:
-                print "\033[93mSet value of Expires.\033[1;m"
+            if key.cache_control is None or key.expires is None:
+                print "\033[1;31mNo Cache Control & Expires headers are set!\033[1;m"
                 metadata = key.metadata
                 metadata['Cache-Control'] = 'max-age=%d, public' % (60 * 60 * 24 * 180) # Set Cache-Control Value to 60 x 60 x 24 x 180 = 6 Months
                 metadata['Expires'] = 'Fri, 15 Feb 2019 16:00:00 GMT' # Setting Expires Metadata date & time.
